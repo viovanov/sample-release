c9c7e7997f6a95449ae6615f4589d848dc8d06a9
vlad.iovanov <vlad.iovanov@uhurusoftware.com>
2014-11-24 10:27:54 -0800

    Add router registration.

--- src/github.com/uhurusoftware/hello-diego/main.go
+++ src/github.com/uhurusoftware/hello-diego/main.go
@@ -8,7 +8,9 @@ import (
 
 	"github.com/apcera/nats"
 	"github.com/cloudfoundry-incubator/cf-lager"
+	"github.com/cloudfoundry/gibson"
 	"github.com/cloudfoundry/gunk/diegonats"
+	"github.com/cloudfoundry/yagnats"
 	"github.com/pivotal-golang/lager"
 	"github.com/tedsuo/ifrit"
 	"github.com/tedsuo/ifrit/grouper"
@@ -41,10 +43,15 @@ var listenAddr = flag.String(
 	"host to listen on for the HTTP viewer",
 )
 
+var routes = flag.String(
+	"routes",
+	"",
+	"routes to register, in the form of uri,uri,uri")
+
 var listenPort = flag.Int(
-        "listenPort",
-        8080,
-        "port to listen on for the HTTP viewer",
+	"listenPort",
+	8080,
+	"port to listen on for the HTTP viewer",
 )
 
 var subscription *nats.Subscription
@@ -67,6 +74,8 @@ func main() {
 
 	ListenOnNATS(natsClient, flowViewer)
 
+	RegisterWithRouter()
+
 	<-monitor.Wait()
 
 	fmt.Println("\nReceived an interrupt, stopping ...\n")
@@ -87,13 +96,47 @@ func ListenOnNATS(client diegonats.NATSClient, flowViewer chan (string)) error {
 	return nil
 }
 
+func RegisterWithRouter() {
+	nats := yagnats.NewClient()
+	natsMembers := []yagnats.ConnectionProvider{}
+
+	for _, addr := range strings.Split(*natsAddresses, ",") {
+		log.Println("configuring nats server:", addr)
+		natsMembers = append(natsMembers, &yagnats.ConnectionInfo{
+			Addr:     addr,
+			Username: *natsUsername,
+			Password: *natsPassword,
+		})
+	}
+
+	natsInfo := &yagnats.ConnectionCluster{natsMembers}
+
+	for {
+		err := nats.Connect(natsInfo)
+		if err == nil {
+			break
+		}
+
+		log.Println("failed to connect to NATS:", err)
+		time.Sleep(1 * time.Second)
+	}
+
+	client := gibson.NewCFRouterClient(*listenAddr, nats)
+
+	client.Greet()
+
+	for _, route := range strings.Split(*routes, ",") {
+		client.Register(listenPort, route)
+	}
+}
+
 func InitializeHTTPServer(logger lager.Logger, flowViewer chan (string)) ifrit.Runner {
 	viewerHandler, err := viewer.NewServer(logger, flowViewer)
 
 	if err != nil {
 		panic("failed to initialize viewer server: " + err.Error())
 	}
-	return http_server.New(*listenAddr, viewerHandler)
+	return http_server.New(*(listenAddr + string(listenPort)), viewerHandler)
 }
 
 func StopListening() error {
